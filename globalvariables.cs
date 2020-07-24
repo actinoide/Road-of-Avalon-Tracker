89a1d572de5d843f7cab2c56bfe14f5b9feae1f9
acti noide <das0schaf@gmx.de>
2020-07-24 14:17:42 +0200

    added class for timer mannagment corrected spelling added comments added basic UI

--- globalvariables.cs
+++ globalvariables.cs
@@ -4,9 +4,9 @@ using System.Text;
 
 namespace albion_avalon
 {
-    public static class globalvariables
+    public static class GlobalVariables
     {
-        public static int updatetime = 60000;//time between updates in ms
-        public static List<albionzonedefinition> visitedzones = new List<albionzonedefinition>();//list of zones 
+        public static int UpdateTime = 60000;//time between updates in ms
+        public static List<AlbionZoneDefinition> VisitedZones = new List<AlbionZoneDefinition>();//list of zones 
     }
 }
