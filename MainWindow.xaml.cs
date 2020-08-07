c2b6aefeb8aa18494239303899dd504c45f465c7
acti noide <das0schaf@gmx.de>
2020-08-07 16:31:56 +0200

    comments i forgot

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -246,13 +246,13 @@ namespace albion_avalon
         {
             if(MessageBox.Show("this will imediately delete all expired portals and zones without portals. it will also delete any portals as soon as they expire and any zones as soon as they are empty. this cannot be undone. are you sure ?","warning",MessageBoxButton.YesNo) == MessageBoxResult.Yes)//asks user for confirmation
             {
-                GlobalVariables.IsInAutoDeleteMode = true;
+                GlobalVariables.IsInAutoDeleteMode = true;//sets the auto delete variable to true
             }
             else
             {
                 return;
             }
-            UIUpdater();
+            UIUpdater();//updaes the ui
         }
     }
 }
--- globalvariables.cs
+++ globalvariables.cs
@@ -6,10 +6,8 @@ namespace albion_avalon
 {
     public static class GlobalVariables
     {
-        public static int TimerInterval = 60000;//time between updates in ms
         public static List<AlbionZoneDefinition> VisitedZones = new List<AlbionZoneDefinition>();//list of zones 
-        public static bool IsInTimerMode = true;//bool to switch between timer and foreground based update modes
-        public static DateTime LastUpdateTime = DateTime.UtcNow;//to keep track of the last time the update function was called on foreground update mode
         public static uint ZoneIDCounter = 0;//counter for zone ids
+        public static bool IsInAutoDeleteMode = false;// bool for auto delete mode
     }
 }
